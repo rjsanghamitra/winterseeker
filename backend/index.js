cfc5aa8c5d1792a27a6276539edde0f1f9fbcce9
Sanghamitra <rjsanghamitra@gmail.com>
2023-10-17 14:28:12 +0530

    fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,26 @@ const path = require("path");
 
 const PORT = process.env.PORT;
 
-app.use(cors({
+app.use(
+  cors({
     origin: "https://winterseeker.vercel.app",
     methods: ["GET"],
     credentials: true,
-}));
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/current-weather", current);
-
-app.get("/", (req, res) => {
-    res.send(`PORT ${PORT} CONNECTED`);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://winterseeker.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
+app.get("/", (req, res) => {
+  res.send(`PORT ${PORT} CONNECTED`);
+});
 
 app.listen(PORT);
